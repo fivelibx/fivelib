8814e0c34b0e8d8aedafd260cbff1a0bdbeaa68c
lucaspaiva-lp <gml248@tutamail.com>
2026-05-01 15:32:47 -0300

    feat: Atualização no health para ter resposta direta do status do banco de dados.

--- backend/api/routes/health.py
+++ backend/api/routes/health.py
@@ -1,12 +1,22 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
+from sqlalchemy.orm import Session
+from sqlalchemy import text
+from backend.database.config import get_db
 
 router = APIRouter(prefix="/health", tags=["health"])
 
 @router.get("/")
-def health_check():
+def health_check(db: Session = Depends(get_db)):
+    try:
+        #NOTE: Executa uma consulta simples para validar a conectividade com o banco
+        db.execute(text("SELECT 1"))
+        db_status = "ok"
+    except Exception as e:
+        #NOTE: Em caso de falha na conexão ou timeout
+        db_status = f"unreachable: {str(e)}"
+    
     return {
         "status": "OK",
-        "database": "untested", #TODO: Atualizar após subir banco está configurado.
-        "service": "running" 
+        "database": db_status,
+        "service": "running"
     }
-
